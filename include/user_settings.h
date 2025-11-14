ae703d6611c626b7516bfe99ccae7c4e206e5cec
James Shawn Carnley <Shawn.Carnley@gatech.edu>
2025-11-14 17:58:36 -0500

    fix: Add comments to Wi-Fi credentials for clarity in user_settings.h

--- include/user_settings.h
+++ include/user_settings.h
@@ -1,8 +1,8 @@
 // ---------- USER SETTINGS ----------
 #pragma once
 // Wi-Fi creds
-#define WIFI_SSID     ""
-#define WIFI_PASSWORD ""
+#define WIFI_SSID     "" // Leave empty to use portal
+#define WIFI_PASSWORD "" // Leave empty to use portal
 
 // Optional compile-time default home (used until phone pushes)
 #define HOME_LAT 33.7501; // Atlanta, GA
